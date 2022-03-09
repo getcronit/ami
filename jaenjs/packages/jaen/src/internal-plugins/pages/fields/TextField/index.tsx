b7e3da028b030baacf4948719827995d32de6a49
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-03-09 18:05:06 +0000

    feat: add styling api to textfield

--- jaenjs/packages/jaen/src/internal-plugins/pages/fields/TextField/index.tsx
+++ jaenjs/packages/jaen/src/internal-plugins/pages/fields/TextField/index.tsx
@@ -1,15 +1,18 @@
+import {Box, BoxProps} from '@chakra-ui/react'
 import Editor from '@jaen/shared/Editor'
 import {connectField} from '../../index'
 
-const TextField = connectField<string, string, {rtf?: boolean}>(
-  ({jaenField, rtf = false}) => (
-    <Editor
-      defaultValue={jaenField.staticValue || jaenField.defaultValue}
-      value={jaenField.value}
-      onBlurValue={data => jaenField.onUpdateValue(data)}
-      editing={jaenField.isEditing}
-      disableToolbar={!rtf}
-    />
+const TextField = connectField<string, string, {rtf?: boolean} & BoxProps>(
+  ({jaenField, rtf = false, ...boxProps}) => (
+    <Box style={jaenField.style} className={jaenField.className} {...boxProps}>
+      <Editor
+        defaultValue={jaenField.staticValue || jaenField.defaultValue}
+        value={jaenField.value}
+        onBlurValue={data => jaenField.onUpdateValue(data)}
+        editing={jaenField.isEditing}
+        disableToolbar={!rtf}
+      />
+    </Box>
   ),
   {fieldType: 'IMA:TextField'}
 )
