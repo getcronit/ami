2d8a9122001f5116f7ebf15a7c12218043e4674e
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-04-22 17:18:11 +0000

    fix: update webpack config

--- jaenjs/packages/storybook-addon-chakra-ui/src/preset.ts
+++ jaenjs/packages/storybook-addon-chakra-ui/src/preset.ts
@@ -5,18 +5,25 @@ module.exports = {
   managerEntries: function (entry = []) {
     return [...entry, require.resolve('./register')]
   },
-  webpackFinal: async (config: any) => {
-    // WA due to: https://github.com/storybookjs/storybook/pull/13300
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@emotion/core': '@emotion/react',
-          'emotion-theming': '@emotion/react'
-        }
-      }
-    }
+  // unpins Storybook's dependence on Emotion 10 so that build can compile successfully
+  features: {emotionAlias: false},
+  webpackFinal: async (
+    config: {module: {rules: {test: RegExp; include: RegExp; type: string}[]}},
+    {configType}: any
+  ) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // https://github.com/polkadot-js/extension/issues/621#issuecomment-759341776
+    // framer-motion uses the .mjs notation and we need to include it so that webpack will
+    // transpile it for us correctly (enables using a CJS module inside an ESM).
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    })
+    // Return the altered config
+    return config
   }
 }
