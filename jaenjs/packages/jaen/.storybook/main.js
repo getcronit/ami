cacffdc4d2053f1eadef7b03bd8fe9be6b2a43a2
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-04-25 13:54:26 +0000

    chore: update storybook config

--- jaenjs/packages/jaen/.storybook/main.js
+++ jaenjs/packages/jaen/.storybook/main.js
@@ -1,23 +1,27 @@
-const path = require('path')
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@snek-at/storybook-addon-chakra-ui',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-addon-gatsby'
+    '@snek-at/storybook-addon-chakra-ui'
   ],
   framework: '@storybook/react',
   core: {builder: 'webpack5'},
-  webpackFinal: async config => {
+  webpackFinal: async (config, {presets}) => {
+    const webpack = await presets.apply('webpackInstance')
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        HAS_REACT_18: JSON.stringify(false)
+      })
+    )
+
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
-    config.module.rules[0].exclude = [/core-js/]
-
     // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-
-    config.resolve.mainFields = ['browser', 'module', 'main']
+    config.module.rules[0].use[0].options.plugins.push(
+      require.resolve('babel-plugin-remove-graphql-queries')
+    )
 
     config.resolve.fallback = {
       ...config.resolve.fallback,
