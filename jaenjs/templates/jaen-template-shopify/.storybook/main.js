05c480d785e66812256b5bb9b8e20b95b5649a81
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-04-26 21:48:58 +0000

    chore: add @jaenjs/jaen to storybook transpilation

--- jaenjs/templates/jaen-template-shopify/.storybook/main.js
+++ jaenjs/templates/jaen-template-shopify/.storybook/main.js
@@ -19,8 +19,10 @@ module.exports = {
       })
     )
 
-    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    // Transpile Gatsby module because Gatsby and Jaen includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [
+      /node_modules\/(?!(gatsby|@jaenjs\/jaen)\/)/
+    ]
     // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
     config.module.rules[0].use[0].options.plugins.push(
       require.resolve('babel-plugin-remove-graphql-queries')
