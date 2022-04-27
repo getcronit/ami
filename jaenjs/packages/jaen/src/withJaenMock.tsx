ad88a35ac155d8384e8670bb98b7e8d52206964e
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-04-27 01:03:55 +0000

    feat: add  notification support to withJaenMock
    also added sticky admintoolbar

--- jaenjs/packages/jaen/src/withJaenMock.tsx
+++ jaenjs/packages/jaen/src/withJaenMock.tsx
@@ -1,3 +1,9 @@
+import {Flex, Box} from '@chakra-ui/react'
+import {
+  NotificationProvider,
+  NotificationProviderProps
+} from './internal-plugins/notify/services/notification/context'
+import {withRedux as withNotificationsProvider} from './internal-plugins/notify/redux'
 import {
   JaenPageContext,
   JaenPageProvider
@@ -9,29 +15,61 @@ import {AdminToolbar} from './ui/components/AdminToolbar'
 import {ToolbarChangesElement} from './ui/ToolbarChangesElement'
 import {withSnekFinder} from './withSnekFinder'
 
+export type JaenMockProps = NotificationProviderProps | JaenPageContext
+
 export const withJaenMock = <P extends object>(
   Component: React.ComponentType<P>,
-  jaenPageProps: JaenPageContext
+  mockProps: JaenMockProps
 ) => {
+  const InnerElement = ({children}: {children: React.ReactNode}) => {
+    if ((mockProps as any).notification) {
+      const props = mockProps as NotificationProviderProps
+
+      const NotificationsProvider = withNotificationsProvider(
+        NotificationProvider
+      )
+
+      return (
+        <NotificationsProvider {...props}>{children}</NotificationsProvider>
+      )
+    }
+
+    if ((mockProps as any).jaenPage) {
+      const props = mockProps as JaenPageContext
+
+      return (
+        <SiteProvider>
+          <JaenPageProvider {...props}>{children}</JaenPageProvider>
+        </SiteProvider>
+      )
+    }
+
+    return <>{children}</>
+  }
+
   const MockedComponent = (props: P) => {
     return (
       <IncomingBuildCheckerProvider>
-        <SiteProvider>
-          <AdminToolbar
-            logoText={'Jaen Mock'}
-            toolbarItems={{
-              left: [
-                <EditButton />,
-                <ToolbarChangesElement />,
-                <DiscardButton />
-              ],
-              right: []
-            }}
-          />
-          <JaenPageProvider {...jaenPageProps}>
-            <Component {...props} />
-          </JaenPageProvider>
-        </SiteProvider>
+        <Flex direction={'column'}>
+          <Box pos="sticky" top="0" zIndex={'banner'}>
+            <AdminToolbar
+              logoText={'Jaen Mock'}
+              toolbarItems={{
+                left: [
+                  <EditButton />,
+                  <ToolbarChangesElement />,
+                  <DiscardButton />
+                ],
+                right: []
+              }}
+            />
+          </Box>
+          <Box>
+            <InnerElement>
+              <Component {...props} />
+            </InnerElement>
+          </Box>
+        </Flex>
       </IncomingBuildCheckerProvider>
     )
   }
