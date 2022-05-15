ed9185a04f94871ff625285c550257ce0ce25f9f
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-05-15 13:04:48 +0000

    add user auth function

---
+++ jaenjs/examples/demo/src/snek-functions/usersAuth.ts
@@ -0,0 +1,58 @@
+import {makeFn} from '@snek-at/functions'
+
+type Sheet = Array<{
+  email: string
+  password: string
+}>
+
+const PROJECT_ID = parseInt(process.env.PROJECT_ID) || 1
+const SHEETS_TOKEN = process.env.SHEETS_TOKEN || 'aaa'
+const SHEET_NAME = 'snek-functions-users'
+
+const usersAuth = makeFn<{email: string; password: string}, boolean>(
+  async (args, snekApi) => {
+    console.log('args', args)
+
+    const {createHash} = await import('crypto')
+
+    // 1. Check if auth sheet exists, and if not, create it with default values
+
+    try {
+      const sheet = await snekApi.getSheet({
+        projectId: PROJECT_ID,
+        sheetsToken: SHEETS_TOKEN,
+        sheetName: SHEET_NAME
+      })
+
+      // file to JSON
+      const json = await sheet.text()
+
+      // JSON to object
+      const obj = JSON.parse(json) as Sheet
+
+      // Find user
+      const user = obj.find(u => u.email === args.email)
+
+      if (user) {
+        // Check password
+
+        const maybePassword = createHash('sha256')
+          .update(args.password)
+          .digest('hex')
+
+        if (user.password === maybePassword) {
+          return true
+        }
+      }
+    } catch {
+      throw new Error(`Sheet ${SHEET_NAME} does not exist`)
+    }
+
+    return false
+  },
+  {
+    name: 'usersAuth'
+  }
+)
+
+export default usersAuth
