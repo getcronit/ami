7efec4fa07e89d7aa8d4f9280a3aa38ef265569a
Florian Kleber <kleberbaum@erebos.xyz>
2022-07-08 17:01:39 +0000

    feat: add pyExample to snek functions example

---
+++ jaenjs/packages/snek-functions/example/my-example-project/src/functions/example.py
@@ -0,0 +1,11 @@
+from sys import argv
+import django
+
+
+def main(a: int, b: int) -> int:
+    print(a + b + 1)
+    return a + b
+
+
+if __name__ == '__main__':
+    main(int(argv[1]), int(argv[2]))
