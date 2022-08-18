848c6bfc212318258b2a911e353507163c6588b1
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-08-18 11:39:17 +0000

    feat: add redirect for invalid paths on dynamic routing

--- jaenjs/packages/jaen/src/internal-plugins/pages/internal/services/routing/pages/dynamic.tsx
+++ jaenjs/packages/jaen/src/internal-plugins/pages/internal/services/routing/pages/dynamic.tsx
@@ -1,81 +1,91 @@
 import {Center, CircularProgress} from '@chakra-ui/react'
-import {useAppSelector, withRedux} from '../../../redux'
-import {useJaenTemplates, useSiteContext} from '../../../services/site'
-import {usePromiseEffect} from '../../../../../../utils/hooks/usePromiseEffect'
 import {RouteComponentProps} from '@reach/router'
 import {navigate, PageProps} from 'gatsby'
 import * as React from 'react'
+import {usePromiseEffect} from '../../../../../../utils/hooks/usePromiseEffect'
+import {useAppSelector, withRedux} from '../../../redux'
+import {useJaenTemplates, useSiteContext} from '../../../services/site'
 
 const Dynamic = (props: RouteComponentProps & Partial<PageProps>) => {
   const dynamicPaths = useAppSelector(
     state => state.internal.routing.dynamicPaths
   )
 
-  const path = props.location?.hash?.substring(1)
-
   if (typeof window === 'undefined') {
     return <>dynamic</>
   }
 
-  if (!path) {
-    throw new Error('Something went wrong while preparing a dynamic page')
-  }
-
-  const {pageId, templateName} = React.useMemo(() => dynamicPaths[path], [path])
-
-  const {templateLoader} = useSiteContext()
+  const path = props.location?.hash?.substring(1)
 
   React.useEffect(() => {
-    // We can determine if a page has been moved by checking if the path is no longer in the dynamicPaths, if so
-    // we search for the the new path in the dynamicPaths by pageId and redirect to it.
-
-    if (!(path in dynamicPaths)) {
-      const newPath = Object.keys(dynamicPaths).find(
-        path => dynamicPaths[path]?.pageId === pageId
-      )
-
-      if (newPath) {
-        // Page has been moved, update to the new path
-        navigate(`/_#${newPath}`)
-      } else {
-        // Page has been deleted, redirect to the parent page
-        navigate('/')
-      }
+    if (!path || !dynamicPaths[path]) {
+      navigate('/')
     }
-  }, [dynamicPaths])
+  }, [path])
 
   const loadingComponent = (
-    <Center>
-      <CircularProgress isIndeterminate color="green.300" />
+    <Center my="24">
+      <CircularProgress isIndeterminate color="teal.300" />
     </Center>
   )
 
-  const templates = useJaenTemplates()
+  const TemplateLoader = ({path}: {path: string}) => {
+    const {pageId, templateName} = dynamicPaths[path]
 
-  // We need to wait for the template to be loaded before we can render the page,
-  // so template null means the template is not loaded yet
-  const template = templates
-    ? templates.find(t => t.name === templateName)
-    : null
+    const {templateLoader} = useSiteContext()
 
-  const {value: Component} = usePromiseEffect(async () => {
-    if (template) {
-      // TODO: Remove this hack to ignore incorrect template names
-      return await templateLoader(templateName)
+    React.useEffect(() => {
+      // We can determine if a page has been moved by checking if the path is no longer in the dynamicPaths, if so
+      // we search for the the new path in the dynamicPaths by pageId and redirect to it.
+
+      if (!(path in dynamicPaths)) {
+        const newPath = Object.keys(dynamicPaths).find(
+          path => dynamicPaths[path]?.pageId === pageId
+        )
+
+        if (newPath) {
+          // Page has been moved, update to the new path
+          navigate(`/_#${newPath}`)
+        } else {
+          // Page has been deleted, redirect to the parent page
+          navigate('/')
+        }
+      }
+    }, [dynamicPaths])
+
+    const templates = useJaenTemplates()
+
+    // We need to wait for the template to be loaded before we can render the page,
+    // so template null means the template is not loaded yet
+    const template = templates
+      ? templates.find(t => t.name === templateName)
+      : null
+
+    const {value: Component} = usePromiseEffect(async () => {
+      if (template) {
+        // TODO: Remove this hack to ignore incorrect template names
+        return await templateLoader(templateName)
+      }
+    }, [template])
+
+    if (!Component) {
+      return loadingComponent
     }
-  }, [template])
 
-  if (!Component) {
+    return (
+      <Component
+        {...(props as any)}
+        pageContext={{...props.pageContext, jaenPageId: pageId}}
+        data={{...props.data, jaenPage: null}}
+      />
+    )
+  }
+
+  if (!path || !dynamicPaths[path]) {
     return loadingComponent
   }
 
-  return (
-    <Component
-      {...(props as any)}
-      pageContext={{...props.pageContext, jaenPageId: pageId}}
-      data={{...props.data, jaenPage: null}}
-    />
-  )
+  return <TemplateLoader path={path} />
 }
 
 export default withRedux(Dynamic)
