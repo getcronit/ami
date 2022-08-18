0a4a01cf31a24a35bb85e3fdad2ada9b1276cb44
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-08-18 09:45:00 +0000

    feat: add cookie parser

--- jaenjs/packages/snek-functions/src/server/app.ts
+++ jaenjs/packages/snek-functions/src/server/app.ts
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
 import {
@@ -53,6 +54,8 @@ export const getApp = async (options: AppOptions) => {
   const app = express()
 
   app.use(cors())
+
+  app.use(cookieParser())
 
   app.use(express.json())
 
