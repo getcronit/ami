7f3b26a4c3920013bb412ede391fdb15fb87de43
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-09-07 09:47:16 +0000

    feat: auto throw errors in client fn

--- jaenjs/packages/snek-functions/src/makeFn.ts
+++ jaenjs/packages/snek-functions/src/makeFn.ts
@@ -12,91 +12,92 @@ export interface MakeFnOptions {
   }>
 }
 
-export const makeFn =
-  ({url, decorators}: MakeFnOptions) =>
-  <FunctionArgs, FunctionReturn>(
-    snekFunction: SnekFunction<FunctionArgs, FunctionReturn>['server'],
-    options: SnekFunction<FunctionArgs, FunctionReturn>['options']
-  ): SnekFunction<FunctionArgs, FunctionReturn> => {
-    const fn: SnekFunction<FunctionArgs, FunctionReturn> = async args => {
-      const {data} = await fn.execute(args)
+export const makeFn = ({url, decorators}: MakeFnOptions) => <
+  FunctionArgs,
+  FunctionReturn
+>(
+  snekFunction: SnekFunction<FunctionArgs, FunctionReturn>['server'],
+  options: SnekFunction<FunctionArgs, FunctionReturn>['options']
+): SnekFunction<FunctionArgs, FunctionReturn> => {
+  const fn: SnekFunction<FunctionArgs, FunctionReturn> = async args => {
+    const {data, errors} = await fn.execute(args)
 
-      return data
+    if (errors) {
+      throw new Error(errors[0].message)
     }
 
-    fn.options = options
-
-    fn.execute = async (args, init) => {
-      const snekApiHeaders = KeyManager.getHeaders()
-
-      console.log('snekApiHeaders', snekApiHeaders)
-
-      try {
-        const res = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            ...snekApiHeaders,
-            ...init?.headers
-          },
-          body: buildGraphqlQueryString({
-            name: options.name,
-            args: {
-              ...args
-            }
-          }),
-          credentials: 'include'
-        })
-
-        const {data, errors} = await res.json()
-
-        return {
-          res: res as any,
-          data: JSON.parse(data[options.name]),
-          errors: errors || []
-        }
-      } catch (err) {
-        console.error(err)
-        return {
-          res: undefined,
-          data: null,
-          errors: [err]
-        }
-      }
-    }
-
-    fn.server = snekFunction
-
-    if (decorators) {
-      fn.globalDecorators = []
-
-      const shouldSkipDecorator = (
-        decorator: Decorator,
-        includes?: string[],
-        excludes?: string[]
-      ) => {
-        if (includes) {
-          return !includes.includes(decorator.name)
-        } else if (excludes) {
-          return excludes.includes(decorator.name)
-        } else {
-          return false
-        }
-      }
-
-      for (const {
-        decorator,
-        includes: inclds,
-        excludes: exclds
-      } of decorators) {
-        if (!shouldSkipDecorator(decorator, inclds, exclds)) {
-          fn.globalDecorators.push(decorator)
-        }
-      }
-    }
-
-    return fn
+    return data
   }
+
+  fn.options = options
+
+  fn.execute = async (args, init) => {
+    const snekApiHeaders = KeyManager.getHeaders()
+
+    console.log('snekApiHeaders', snekApiHeaders)
+
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          ...snekApiHeaders,
+          ...init?.headers
+        },
+        body: buildGraphqlQueryString({
+          name: options.name,
+          args: {
+            ...args
+          }
+        }),
+        credentials: 'include'
+      })
+
+      const {data, errors} = await res.json()
+
+      return {
+        res: res as any,
+        data: JSON.parse(data[options.name]),
+        errors: errors || []
+      }
+    } catch (err) {
+      console.error(err)
+      return {
+        res: undefined,
+        data: null,
+        errors: [err]
+      }
+    }
+  }
+
+  fn.server = snekFunction
+
+  if (decorators) {
+    fn.globalDecorators = []
+
+    const shouldSkipDecorator = (
+      decorator: Decorator,
+      includes?: string[],
+      excludes?: string[]
+    ) => {
+      if (includes) {
+        return !includes.includes(decorator.name)
+      } else if (excludes) {
+        return excludes.includes(decorator.name)
+      } else {
+        return false
+      }
+    }
+
+    for (const {decorator, includes: inclds, excludes: exclds} of decorators) {
+      if (!shouldSkipDecorator(decorator, inclds, exclds)) {
+        fn.globalDecorators.push(decorator)
+      }
+    }
+  }
+
+  return fn
+}
 
 export const browserMakeFn: typeof makeFn = args => (snekFunction, options) => {
   const fn = makeFn(args)(snekFunction, options)
